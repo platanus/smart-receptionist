f93ed12bd3558133a30319d57470fda50d58e6d9
imarquezc <ignaciomarquezc@gmail.com>
2019-04-08 08:11:33 -0400

    chore(annotate): add annotate gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id           :bigint(8)        not null, primary key
+#  slack_id     :string
+#  is_admin     :string
+#  is_bot       :string
+#  name         :string
+#  last_name    :string
+#  email        :string
+#  image_42     :string
+#  real_name    :string
+#  display_name :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class User < ApplicationRecord
 end
