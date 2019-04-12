6442a3d3d8e4e426ff00467e17359a015e0b1116
imarquezc <ignaciomarquezc@gmail.com>
2019-04-12 11:41:27 -0400

    Initial commit

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,24 @@
 require_relative 'boot'
 
 require 'rails/all'
-
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SmartReceptionist
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+          headers: :any,
+          expose: ['X-Page', 'X-PageTotal'],
+          methods: [:get, :post, :delete, :put, :options]
+      end
+    end
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.fallbacks = [:es, :en]
+    config.i18n.default_locale = 'es-CL'
+    config.active_job.queue_adapter = :sidekiq
+    config.assets.paths << Rails.root.join('node_modules')
+    config.load_defaults 5.2
   end
 end
