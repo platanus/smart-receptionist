ad0a3c614def31132537db5ab35647a387883b92
imarquezc <ignaciomarquezc@gmail.com>
2019-04-12 16:57:34 -0400

    feat(notifications): move notification controller to api scope

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   scope path: '/api' do
     api_version(module: "Api::V1", path: { value: "v1" }, defaults: { format: 'json' }) do
       resources :users, only: :index
+      resources :notifications, only: :create
     end
   end
   devise_for :admin_users, ActiveAdmin::Devise.config
@@ -9,5 +10,4 @@ Rails.application.routes.draw do
   devise_for :users
   mount Sidekiq::Web => '/queue'
   root 'application#index'
-  resources :notifications, only: :create
 end
