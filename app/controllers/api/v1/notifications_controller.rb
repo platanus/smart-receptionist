58f33d2caa26fbf8e15e9a9e626263288d942ea6
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2019-06-07 16:54:03 -0400

    fix(): fixed channel name retrieve

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::NotificationsController < ApplicationController
   protect_from_forgery except: :create
-  
+
   CHANNELS = {
     buda: ENV.fetch('BUDA_CHANNEL_NAME'),
     fintual: ENV.fetch('FINTUAL_CHANNEL_NAME'),
@@ -12,7 +12,7 @@ class Api::V1::NotificationsController < ApplicationController
       slack.notify_user(User.find(notification_params[:user_id]))
     else
       slack.notify_channel(
-        CHANNELS[notification_params[:company_name]], 
+        CHANNELS[notification_params[:company_name].to_sym],
         notification_params[:message])
     end
   end
