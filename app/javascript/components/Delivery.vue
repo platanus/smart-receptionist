29b61a177c6a6900688d8c799afd73255c7f21e2
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2019-06-28 12:41:19 -0400

    feat(delivery): show users in side column

--- app/javascript/components/Delivery.vue
+++ app/javascript/components/Delivery.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <div v-if="!timer">
-      <div class="header">
-        <div class="header__title">¿A quién buscas?</div>
-        <div class="header__subtitle">Escribe su nombre o búscalo</div>
-        <button class="header__back" v-on:click="back()"></button>
+    <div v-if="!timer" class="delivery">
+      <div class="column column--margin-top">
+        <div class="header">
+          <div class="header__title">¿A quién buscas?</div>
+          <div class="header__subtitle">Escribe su nombre o búscalo</div>
+          <button class="header__back" v-on:click="back()"></button>
+        </div>
+        <div class="search">
+          <input v-model="query" type="text" class="search__bar" autofocus>
+        </div>
       </div>
-      <div class="search">
-        <input v-model="query" type="text" class="search__bar" autofocus>
-      </div>
-      <div class="people">
+      <div class="column column--big">
+        <div class="people">
         <div class="user-card" v-on:click="notifyUser(user)" v-bind:key="user.id" v-for="user in filteredUsers">
           <img class="user-card__avatar" :src="user.image72">
           <div class="user-card__data">
             <div class="user-card__name">{{ user.realName }}</div>
           </div>
+        </div>
         </div>
       </div>
     </div>
@@ -55,7 +59,7 @@ export default {
   watch: {
     query() {
       this.filteredUsers = this.users.filter((user) => {
-        return (user.name + user.email).toLowerCase().indexOf(this.query.toLowerCase()) >= 0;
+        return (user.realName + user.email).toLowerCase().indexOf(this.query.toLowerCase()) >= 0;
       });
     },
   }
@@ -63,10 +67,32 @@ export default {
 </script>
 
 <style lang="scss">
+body {
+  overflow: hidden;
+}
+
+.delivery {
+  display: flex;
+}
+
 .people {
   display: flex;
   flex-wrap: wrap;
-  justify-content: center;
+  flex: 4;
+  overflow-y: scroll;
+  max-height: 100vh;
+}
+
+.column {
+  flex: 3;
+
+  &--big {
+    flex: 4;
+  }
+
+  &--margin-top {
+    margin-top: 35px;
+  }
 }
 
 .search {
@@ -78,8 +104,8 @@ export default {
   height: 60px;
 
   &__bar {
-    border: none;
-    border-bottom: solid 1.5px #3210D3;
+    border: 0;
+    border-bottom: solid 1.5px #3210d3;
     text-align: center;
     width: 80%;
     font-size: 42px;
@@ -93,7 +119,7 @@ export default {
   height: 60px;
   padding: 10px;
   margin: 10px;
-  width: 40%;
+  width: calc(50% - 42px);
 
   &__avatar {
     border-radius: 50%;
