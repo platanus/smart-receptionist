1c14157d79e3fbf774594f1964b3d250e1bf0981
Juan Ignacio Márquez <ignaciomarquezc@gmail.com>
2019-10-04 12:13:29 -0300

    feat(): use public hass api intead of local

--- app/controllers/api/v1/door_actions_controller.rb
+++ app/controllers/api/v1/door_actions_controller.rb
@@ -1,10 +1,11 @@
 class Api::V1::DoorActionsController < ApplicationController
   CODE = ENV.fetch('MAIN_DOOR_CODE')
+  API_KEY = ENV.fetch('HASS_API_KEY')
 
   def open_main_door
     if CODE == params[:code]
       HTTParty.post(
-        'http://hass-local.platan.us:8123/api/services/switch/turn_on',
+        "http://hass.platan.us:8123/api/services/switch/turn_on?api_password=#{API_KEY}",
         body: { entity_id: 'switch.main_door' }.to_json,
         headers: { 'Content-Type' => 'application/json', 'Accept' => 'application/json' }
       )
